f8fea84f27f73aa9fc1924835a2743b5c73ba219
basheerbalogun <bbalogun@afexnigeria.com>
2022-11-01 09:24:52 +0100

    jobs models created

--- jobs/models.py
+++ jobs/models.py
@@ -1,7 +1,31 @@
 from django.db import models
-
-# Create your models here.
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
+from django.urls import reverse
+from django.utils.text import slugify
 
 
 class Job(models.Model):
-    pass
+    title = models.CharField(max_length=200)
+    slug = models.SlugField()
+    description = models.TextField()
+    responsibilities = models.TextField()
+    requirement = models.TextField()
+    experience = models.CharField(max_length=250)
+    date_posted = models.DateTimeField(auto_now_add=True)
+    deadline = models.DateTimeField()
+
+    class Meta:
+        ordering = ('-date_posted',)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('job-detail', args=[self.pk])
+
+
+@receiver(pre_save, sender=Job)
+def slugify_title(sender, **kwargs):
+    slug = slugify(kwargs['instance'].title)
+    kwargs['instance'].slug = slug
