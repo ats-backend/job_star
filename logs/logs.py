f9aa7fcbf640527e566ded78597414a6d08272e0
basheerbalogun <bbalogun@afexnigeria.com>
2022-11-09 14:05:04 +0100

    JobSerializer modified

--- logs/logs.py
+++ logs/logs.py
@@ -1,21 +1,15 @@
 import logging
 
 
-def log_writter():
+def log_writter(action):
 
-    if logging.DEBUG:
-        logging.basicConfig(
-            filename="log.txt",
-            level=logging.DEBUG,
-            format="%(asctime)s %(message)s"
-        )
-        return logging
-    elif logging.info:
-        logging.basicConfig(
-            filename="log.txt",
-            level=logging.DEBUG,
-            format="%(asctime)s %(message)s"
-        )
-        print(logging)
-        return logging.info("The program is working as expected")
+    # if logging.DEBUG:
+    #     logging.basicConfig(
+    #         filename="log.txt",
+    #         level=logging.DEBUG,
+    #         format="%(asctime)s %(message)s"
+    #     )
+    #     return logging
+
+    return logging.info(f"{action}")
 
