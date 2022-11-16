cacf09274ca593a080aafbe33058d6d9ac4c68d7
Lukman Abisoye <25963466+luqmanAA@users.noreply.github.com>
2022-11-16 11:56:03 +0100

    applications url modified

--- job_star/urls.py
+++ job_star/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('', include('applications.urls')),
+    path('applications/', include('applications.urls')),
     path('jobs/', include('jobs.urls')),
     # path('logs/', include('logs.urls')),
     path('admin/', admin.site.urls),
