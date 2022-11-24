d08fa8dc5a3f6ca79dab2f9b88aeacba9fbfffbc
basheerbalogun <bbalogun@afexnigeria.com>
2022-11-24 14:35:45 +0100

    cohort update serializer modified

--- jobs/serializers.py
+++ jobs/serializers.py
@@ -215,6 +215,33 @@ class CohortUpdateSerializer(serializers.ModelSerializer):
             )
         return attrs
 
+    def update(self, instance, validated_data):
+        courses = validated_data.pop('courses')
+        instance.name = validated_data.get('name', instance.name)
+        instance.application_start_date = validated_data.get(
+            'application_start_date', instance.application_start_date)
+        instance.application_end_date = validated_data.get(
+            'application_start_end', instance.application_end_date)
+        instance.start_date = validated_data.get(
+            'start_date', instance.start_date)
+        instance.end_date = validated_data.get(
+            'end_date', instance.end_date)
+
+        courses_id = []
+
+        try:
+            for course in courses:
+                for d in course:
+                    course_id = Courses.objects.get(title=(course[d]))
+                    courses_id.append(course_id.pk)
+                    print(courses_id)
+                    instance.courses.add(courses_id[0])
+            instance.save()
+            return instance
+        except:
+            raise Exception({
+                'An error occurred'
+            })
 
 class JobListSerializers(serializers.ModelSerializer):
     # application = serializers.HyperlinkedIdentityField(
