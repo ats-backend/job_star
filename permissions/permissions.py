cf25e0194baeb647ad825d7e4ff420e316b5e752
Lukman Abisoye <25963466+luqmanAA@users.noreply.github.com>
2022-11-25 12:14:50 +0100

    permissions.py modified

--- permissions/permissions.py
+++ permissions/permissions.py
@@ -18,13 +18,21 @@ class IsAuthenticated(BasePermission):
             )
         local_api_key = config('API_KEY')
         local_secret_key = config('SECRET_KEY')
+        local_api_key_for_website = config('API_KEY_2')
+        local_secret_key_for_website = config('SECRET_KEY_2')
         try:
-            de_hash = local_api_key + local_secret_key + request_ts
-        except:
-            return False
-        hash = hashlib.sha256(de_hash.encode('utf8')).hexdigest()
+            if API_KEY == local_api_key:
+                de_hash = local_api_key + local_secret_key + request_ts
+            elif API_KEY == local_api_key_for_website:
+                de_hash = local_api_key_for_website + local_secret_key_for_website + request_ts
+            else:
+                de_hash = None
+            hash = hashlib.sha256(de_hash.encode('utf8')).hexdigest()
 
-        if hash != hash_key:
+        except:
+            raise AuthenticationFailed()
+
+        if hash_key != hash:
             raise AuthenticationFailed()
 
         return True
