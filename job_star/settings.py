ccca18f38ad95eb7466a522cc76152189d438da8
Lukman Abisoye <25963466+luqmanAA@users.noreply.github.com>
2022-11-27 17:01:12 +0100

    cors headers modified

--- job_star/settings.py
+++ job_star/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 import os
 
+from corsheaders.defaults import default_headers
 from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -183,6 +184,11 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1",
     "http://assessbk.afexats.com",
+]
+CORS_ALLOW_HEADERS = list(default_headers) + [
+    'request-ts',
+    'api-key',
+    'hash-key'
 ]
 
 EMAIL_BACKEND = 'djcelery_email.backends.CeleryEmailBackend'
